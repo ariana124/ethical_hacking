85042d4a84c3296f310798d5c8eb506c04fd23eb
ariana124 <ariana.bibiano@gmail.com>
2020-10-23 12:57:08 -0700

    changes mac address using user input

---
+++ mac_address_changer/mac_changer_user_input.py
@@ -0,0 +1,18 @@
+
+#!/usr/bin/env python
+"""
+Program that changes the MAC address of a system using user input.
+"""
+
+import subprocess
+
+
+interface = raw_input("Interface > ")
+new_mac = raw_input("New MAC > ")
+
+
+subprocess.call("ifconfig " + interface + " down", shell=True)
+subprocess.call("ifconfig " + interface + " hw ether" + new_mac, shell=True)
+subprocess.call("ifconfig " + interface + " up", shell=True)
+
+print("[+] The MAC address of " + interface + " has been changed to " + new_mac)
