01c770ebe09838b1d19d25e8923f3c6bf2ddff8e
ariana124 <ariana.bibiano@gmail.com>
2020-10-26 13:03:14 -0700

    a more secure method of changing the mac address

---
+++ mac_address_changer/secure_mac_changer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+"""
+Program that changes the MAC address of a system using user input.
+"""
+
+import subprocess
+
+
+interface = raw_input("Interface > ")
+new_mac = raw_input("New MAC > ")
+
+
+subprocess.call(["ifconfig", interface, "down"])
+subprocess.call(["ifconfig", interface, "hw ether", new_mac])
+subprocess.call(["ifconfig", interface, "up"])
+
+print("[+] The MAC address of " + interface + " has been changed to " + new_mac)
