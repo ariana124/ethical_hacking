cff375d40adddee3b50d15226ba3ed7b5212fe6c
ariana124 <ariana.bibiano@gmail.com>
2020-10-28 12:34:23 -0700

    updated comments

--- mac_address_changer/cl_args_mac_changer.py
+++ mac_address_changer/cl_args_mac_changer.py
@@ -6,13 +6,13 @@ Program that changes the MAC address of a system using user input.
 import subprocess
 import optparse
 
-# Creates an instance of the OptionParse class.
+# Creates an instance of the OptionParse class that's used to handle user input.
 parser = optparse.OptionParse()
 
 parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
 parser.add_option("-m", "--mac", dest="new_mac", help="New MAC Address")
 
-# Parses the arguments on the command line.
+# Parses the arguments on the command line and stores the arguments into the options variable and the value of it into the argument variable.
 (options, arguments) = parser.parse_args()
 
 interface = options.interface
