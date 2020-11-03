c179ee8381211f4c2aa7c8061a6963daf812d127
ariana124 <ariana.bibiano@gmail.com>
2020-11-03 13:16:54 -0800

    changes mac address using seperate functions

---
+++ mac_address_changer/mac_changer_function.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+"""
+Program that changes the MAC address of a system using user input.
+"""
+
+import subprocess
+import optparse
+
+
+def change_mac(interface, new_mac):
+    subprocess.call(["ifconfig", interface, "down"])
+    subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+    subprocess.call(["ifconfig", interface, "up"])
+    print("[+] The MAC address of " + interface + " has been changed to " + new_mac)
+
+def arg_parser():
+    # Creates an instance of the OptionParse class that's used to handle user input.
+    parser = optparse.OptionParser()
+    parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
+    parser.add_option("-m", "--mac", dest="new_mac", help="New MAC Address")
+    return arg_parser
+
+# Parses the arguments on the command line and stores the arguments into the arguments variable and the value of it into the options variable.
+(options, arguments) = parser.parse_args()
+
+change_mac(options.interface, options.new_mac)
