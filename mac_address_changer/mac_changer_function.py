27e593dd5bacdbbbadcca750d11eee9faf02a2a3
ariana124 <ariana.bibiano@gmail.com>
2020-11-03 13:25:43 -0800

    check to make sure user is inputting the interface and new mac address

--- mac_address_changer/mac_changer_function.py
+++ mac_address_changer/mac_changer_function.py
@@ -7,18 +7,24 @@ import subprocess
 import optparse
 
 
+def arg_parser():
+    # Creates an instance of the OptionParse class that's used to handle user input.
+    parser = optparse.OptionParser()
+    parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
+    parser.add_option("-m", "--mac", dest="new_mac", help="New MAC Address")
+    
+    if not options.interface:
+        parser.error("[-] Please specify an interface, use --help for more info")
+    if not options.new_mac:
+        parser.error("[-] Please specify a new MAC address, use --help for more info")
+    return options
+
 def change_mac(interface, new_mac):
     subprocess.call(["ifconfig", interface, "down"])
     subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
     subprocess.call(["ifconfig", interface, "up"])
     print("[+] The MAC address of " + interface + " has been changed to " + new_mac)
 
-def arg_parser():
-    # Creates an instance of the OptionParse class that's used to handle user input.
-    parser = optparse.OptionParser()
-    parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
-    parser.add_option("-m", "--mac", dest="new_mac", help="New MAC Address")
-    return arg_parser
 
 # Parses the arguments on the command line and stores the arguments into the arguments variable and the value of it into the options variable.
 (options, arguments) = parser.parse_args()
